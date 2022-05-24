9190b8e492f5ecf729a2f2078ffccb2403cdc7ee
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-05-24 12:32:50 -0500

    clase7 backend

--- tercer_bimestre/frontend III/clase4/src/Info.jsx
+++ tercer_bimestre/frontend III/clase4/src/Info.jsx
@@ -1,52 +1,49 @@
-import React from 'react';
-import { personalInformation } from './personalData';
-import List from './List';
-import './infoStyle.css';
+import React from "react";
+import { personalInformation } from "./personalData";
+import List from "./List";
+import "./infoStyle.css";
 
-const Info = ({name, nationality, age, occupation, avatar, lastname, bio}) => {
-    
-    const {experience, education} = personalInformation;
-    return (
-        <div className="info">
-        <div className="avatar">
-            <img src={avatar} alt="" />
-        </div>
-        <div>
-            <h2>Personal Information:</h2>
-            <p>Name: {name}</p>
-            <p>lastname: {lastname}</p>
-            <p>Nationality: {nationality}</p>
-            <p>Age: {age}</p>
-            <p>Ocuppation: {occupation}</p>
+const Info = ({
+  name,
+  nationality,
+  age,
+  occupation,
+  avatar,
+  lastname,
+  bio,
+}) => {
+  const { experience, education } = personalInformation;
+  return (
+    <div className="info">
+      <div className="avatar">
+        <img src={avatar} alt="" />
+      </div>
+      <div>
+        <h2>Personal Information:</h2>
+        <p>Name: {name}</p>
+        <p>lastname: {lastname}</p>
+        <p>Nationality: {nationality}</p>
+        <p>Age: {age}</p>
+        <p>Ocuppation: {occupation}</p>
 
-            <p>experience in movies: </p>
-            <ul>
-                {
-                    experience.map(item => 
-                        <List 
-                            key={item.id}
-                            text={item.movie}
-                        />
-                    )
-                }
-            </ul>
+        <p>experience in movies: </p>
+        <ul>
+          {experience.map((item) => (
+            <List key={item.id} text={item.movie} />
+          ))}
+        </ul>
 
-            <p>Education: </p>
-            <ul>
-                {
-                    education.map(item => 
-                        <List 
-                            key={item.id}
-                            text={item.institute}
-                        />
-                    )
-                }
-            </ul>
+        <p>Education: </p>
+        <ul>
+          {education.map((item) => (
+            <List key={item.id} text={item.institute} />
+          ))}
+        </ul>
 
-            <p className="bio" >Bio: {bio}</p>   
-        </div>
-    </div>        
-    );
-}
+        <p className="bio">Bio: {bio}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Info;
